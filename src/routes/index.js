ac4ead1853bf40917cf811675c7980301278fddd
Casey Choiniere <caseychoiniere@gmail.com>
2018-03-27 13:21:27 -0700

    Add notfound redirect

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import { Container } from 'react-grid-system';
 import Header from '../components/Header';
 import Home from '../containers/Home.jsx';
@@ -11,7 +11,7 @@ export default () => (
             <Container fluid className="mainContainer" id="mc1">
                 <Switch>
                     <Route exact path='/' component={Home} />
-                    <Route path="*" component={()=>(<div>NoMatch</div>)} />
+                    <Redirect from="*" to="/" />
                 </Switch>
             </Container>
         </div>
